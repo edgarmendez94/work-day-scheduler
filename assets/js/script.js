9aa6fae673bf88690447ce8ef6cdf5ed281aea06
Edgar Mendez <edgar.ym26@gmail.com>
2022-05-19 23:57:40 -0400

    trying to get the js done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,25 @@
+var saveButton = document.querySelectorAll(".btn btn-primary");
+var displayEventText = document.getElementById("#event-saved");
+
+//  for grey class="table-secondary"
+// for red class="table-danger"
+// for green class="table-success"
+
 
 // for the moment current date & time data and formatting 
 var reformatDate = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(reformatDate);
+
+
+saveButton.addEventListener("click", function(event) {
+    event.preventDefault();
+
+ // DISPLAY appointment added to local storage
+
+
+
+// add text to local storage
+
+
+
+})
